882e99d0dd2f9d97ed5517931ed637516c130d06
Harikrishnan KK <angularcity@gmail.com>
2019-09-28 00:05:01 -0400

    Updates

---
+++ precache-manifest.1a44bb61f6de9d0e8abe7f245515a0c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "758c62e7c87121441179e27de7804bb7",
+    "url": "/react-zero-to-mastery/index.html"
+  },
+  {
+    "revision": "7a6d11eb7001969dbf6a",
+    "url": "/react-zero-to-mastery/static/css/main.952662ab.chunk.css"
+  },
+  {
+    "revision": "6b610ca0911a0dcf307b",
+    "url": "/react-zero-to-mastery/static/js/2.84fd0de3.chunk.js"
+  },
+  {
+    "revision": "7a6d11eb7001969dbf6a",
+    "url": "/react-zero-to-mastery/static/js/main.c573bbeb.chunk.js"
+  },
+  {
+    "revision": "20d827474a4cb91fb1fb",
+    "url": "/react-zero-to-mastery/static/js/runtime-main.4f5016b3.js"
+  }
+]);
